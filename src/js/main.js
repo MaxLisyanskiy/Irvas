e0d8f765f665f863508cf09189d1505d79033cce
Max <max_lisyanskiy@mail.ru>
2020-07-20 17:13:42 +0300

    Timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,19 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import images from './modules/images';
+import timer from './modules/timer'; 
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
+
+    let deadline = '2020-08-20';
+
+
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms();
     images();
+    timer('.container1', deadline);
 });
