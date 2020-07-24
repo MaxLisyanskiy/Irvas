2879cc5e9653c0058d30ed753d93f5b2d13b96ea
Max <max_lisyanskiy@mail.ru>
2020-07-24 21:47:05 +0300

    ModalFinish

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,21 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import images from './modules/images';
 import timer from './modules/timer'; 
+import changeModalState from './modules/changeModalState';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
+    let modalState = {};
+
     let deadline = '2020-08-20';
 
-
+    changeModalState(modalState);
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    forms();
+    forms(modalState);
     images();
     timer('.container1', deadline);
 });
